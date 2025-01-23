466bdf891d3f8107515917b9c0e36165d768f0cc
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-01-23 14:11:10 +0600

    Beans using XML

--- src/main/java/org/noman/myschool/Main.java
+++ src/main/java/org/noman/myschool/Main.java
@@ -3,12 +3,13 @@ package org.noman.myschool;
 
 import org.noman.myschool.beans.Vehicle;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-//        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-//        Vehicle bean = context.getBean(Vehicle.class);
-//        System.out.println(bean.getName());
+        var context = new ClassPathXmlApplicationContext("beans.xml");
+        Vehicle bean = context.getBean(Vehicle.class);
+        System.out.println(bean.getName());
     }
 }
